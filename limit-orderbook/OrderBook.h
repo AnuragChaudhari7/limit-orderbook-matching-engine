fbd5533fa39cbfb7822ed419ac17b0fe19a201e8
AnuragChaudhari7 <a1chaudhari@ucsd.edu>
2025-06-23 19:50:55 +0100

    implement OrderBook class

---
+++ limit-orderbook/OrderBook.h
@@ -0,0 +1,27 @@
+#ifndef ORDERBOOK_H
+#define ORDERBOOK_H
+
+#include <map>
+#include <deque>
+
+#include "Order.h"
+using namespace std;
+
+class OrderBook {
+    private: //NOTE: Extension plans: if you use a matching engine class, the derivative class needs access to books (protected)
+        /*Fields*/
+        map<double, deque<Order>> bid_book;
+        map<double, deque<Order>> ask_book;
+        //NOTE: In future, replace with custom trees instead of slow maps here (despite RBT)
+    
+    public:
+        /*Constructor*/
+        OrderBook();
+
+        /*Methods*/
+        //NOTE: submitLimitOrder(), cancelLimitOrder(), updateLimitOrder(), go here as functions
+        unsigned int submitLimitOrder(unsigned int quantity, bool type, double price);
+
+};
+
+#endif

2e66136c7d79eb9c1696ae9488c6df8617243755
AnuragChaudhari7 <a1chaudhari@ucsd.edu>
2025-06-23 19:23:18 +0100

    add inclusion guards

--- limit-orderbook/Order.h
+++ limit-orderbook/Order.h
@@ -1,10 +1,18 @@
-class Order{
-    /*Fields all orders should have*/
-    unsigned int id; //TODO: how to generate unique IDs?
-    unsigned long int timestamp; //TODO: What do we use? Unix Epoch?
-    unsigned int quantity;
-    bool type; //TODO: Should it be bool? its basically an int (0: buy, 1: sell)
+#ifndef ORDER_H
+#define ORDER_H
 
-    /*Constructor*/
-    Order(unsigned int _id, unsigned long int _timestamp, unsigned int _quantity, bool _type);
+class Order{
+    private:
+        /*Fields all orders should have*/
+        unsigned int id; //TODO: how to generate unique IDs?
+        unsigned long int timestamp; //TODO: What do we use? Unix Epoch?
+        unsigned int quantity;
+        bool type; //TODO: Should it be bool? its basically an int (0: buy, 1: sell)
+
+    public:
+        /*Constructor*/
+        Order(unsigned int _id, unsigned long int _timestamp, unsigned int _quantity, bool _type);
+        //TODO: Getters & setters for private fields
 };
+
+#endif

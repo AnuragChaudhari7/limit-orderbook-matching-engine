ca22d0f5ea3c436f978818e5f4a711814cb3bc94
AnuragChaudhari7 <a1chaudhari@ucsd.edu>
2025-06-28 09:58:39 +0100

    return type to int for better test case debugging

--- tests/OrderBookTests.cpp
+++ tests/OrderBookTests.cpp
@@ -7,15 +7,26 @@
 //     EXPECT_EQ(1, 1) << "You isda foool";
 // }
 
-//TODO: Change from bool to int. Give certain codes for certain errors. Use GTest for diff. assertions by case.
-bool books_equal(map<double, deque<shared_ptr<Order>>> expected_book, map<double, deque<shared_ptr<Order>>> book){
+/**
+ * Method to test if 2 books match.
+ * @return 
+ *  0: equal
+ *  1: difference in price of the levels
+ *  2: order's price doesn't match its price level
+ *  3: 0 quantity order not deleted
+ *  4: incorrect order type in book
+ *  5: order quantity not equal
+ *  6: difference in # of orders in list for price level
+ *  7: difference in # of price levels 
+ */
+int books_equal(map<double, deque<shared_ptr<Order>>> expected_book, map<double, deque<shared_ptr<Order>>> book){
     /*Iterators for the price levels*/
     auto expected_it = expected_book.begin();
     auto it = book.begin();
 
     while(expected_it != expected_book.end() && it != book.end()){
         if(expected_it->first != it->first){
-            return false; //price levels don't match
+            return 1; //price levels don't match
         }
 
         /*Get list of orders*/
@@ -33,20 +44,20 @@ bool books_equal(map<double, deque<shared_ptr<Order>>> expected_book, map<double
             auto expected_order = dynamic_pointer_cast<LimitOrder>(*(expected_orders_it));
 
             if(order->get_price() != it->first){
-                return false; // order price doesnt match its price level
+                return 2; // order price doesnt match its price level
             }
 
             if(order->get_quantity() <= 0){
-                return false; //quantity <= 0, order shouldve been deleted
+                return 3; //quantity <= 0, order shouldve been deleted
             }
 
-            //NOTE: This isn't needed. Price levels already checked. Decide!
-            if(expected_order->get_price() != order->get_price()){
-                return false; //compared orders don't have same price
+            //TODO: Check if order type in actual matches expected
+            if(expected_order->get_type() !=order->get_type()){
+                return 4;
             }
 
             if(expected_order->get_quantity() != order->get_quantity()){
-                return false; //compared orders don't have same quantity
+                return 5; //compared orders don't have same quantity
             }
 
             expected_orders_it++;
@@ -54,7 +65,7 @@ bool books_equal(map<double, deque<shared_ptr<Order>>> expected_book, map<double
         }
 
         if(!(expected_orders_it == expected_orders.end() && orders_it == orders.end())){
-            return false; //both iterators didn't reach end (one of them has one more order => not same)
+            return 6; //both iterators didn't reach end (one of them has one more order => not same)
         }
 
         expected_it++;
@@ -62,17 +73,17 @@ bool books_equal(map<double, deque<shared_ptr<Order>>> expected_book, map<double
     }
 
     if(!(expected_it == expected_book.end() && it == book.end())){
-        return false; //both iterators didn't reach end (one of them has one more price level => not same)
+        return 7; //both iterators didn't reach end (one of them has one more price level => not same)
     }
 
-    return true;
+    return 0;
 }
 
 //TODO: Test the comparison function
 TEST(BookEquality, EmptyBooks){
     map<double, deque<shared_ptr<Order>>> expected_book;
     map<double, deque<shared_ptr<Order>>> book;
-    EXPECT_EQ(books_equal(expected_book, book), true);
+    EXPECT_EQ(books_equal(expected_book, book), 0);
 }
 
 TEST(BookEquality, OneBuyLimitOrder){

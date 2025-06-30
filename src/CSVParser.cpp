8800cf95bae1746294a7c7706d423b5cd259d1d5
AnuragChaudhari7 <a1chaudhari@ucsd.edu>
2025-06-30 11:54:19 +0100

    cmake config and data folder

--- src/CSVParser.cpp
+++ src/CSVParser.cpp
@@ -3,6 +3,12 @@
 CSVParser::CSVParser(){}
 
 int CSVParser::read_csv(string path_to_csv) {
-    
+    ifstream file;
+    string line = "";
+    file.open(path_to_csv);
+    while(getline(file, line)){
+        cout << line << endl;
+        
+    }
     return 0;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -4,30 +4,29 @@
 #include "OrderBook.h"
 #include "Order.h"
 #include "LimitOrder.h"
+#include "CSVParser.h"
 using namespace std;
 
 int main(){
     //cout << "Its ALIVEE" << endl;
     OrderBook book = OrderBook();
+    CSVParser parser = CSVParser();
+    parser.read_csv("data/example1.csv");
     
-    bool type = 0; //0: buy
-    double price = 110;
-    unsigned int quantity = 10;
+    // bool type = 0; //0: buy
+    // double price = 110;
+    // unsigned int quantity = 10;
 
-    book.submitLimitOrder(type, quantity, price); //0: buy
-    cout << "Bid Order Quantity: " << book.get_bid_book()[price].front()->get_quantity() << endl;
-    cout << "Bid Order Price: $" << dynamic_pointer_cast<LimitOrder>(book.get_bid_book()[price].front())->get_price() << endl;
-
-    type = 1; //1: sell
-    price = 110;
-    quantity = 15;
-    book.submitLimitOrder(type, quantity, price); //1: sell
-    cout << "Ask Order Quantity: " << book.get_ask_book()[price].front()->get_quantity() << endl;
-    cout << "Ask Order Price: $" << dynamic_pointer_cast<LimitOrder>(book.get_ask_book()[price].front())->get_price() << endl;
-
-    // /*Task Failed Succesfully - seg fault here since buy order got fully filled*/
+    // book.submitLimitOrder(type, quantity, price); //0: buy
     // cout << "Bid Order Quantity: " << book.get_bid_book()[price].front()->get_quantity() << endl;
     // cout << "Bid Order Price: $" << dynamic_pointer_cast<LimitOrder>(book.get_bid_book()[price].front())->get_price() << endl;
+
+    // type = 1; //1: sell
+    // price = 110;
+    // quantity = 15;
+    // book.submitLimitOrder(type, quantity, price); //1: sell
+    // cout << "Ask Order Quantity: " << book.get_ask_book()[price].front()->get_quantity() << endl;
+    // cout << "Ask Order Price: $" << dynamic_pointer_cast<LimitOrder>(book.get_ask_book()[price].front())->get_price() << endl;
 
     return 0;
 }
